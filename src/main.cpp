72e11199101589cbdb22ff461d948a53f792fa22
Chris Muga <chrismuga94@gmail.com>
2023-08-25 19:50:24 +0300

    http.h

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,8 @@ using namespace std;
 int main(){
   cout << APP_NAME << "\n";
 
-  /* string response = httpGet(); */
-  /* cout << response << std::endl; */
-
   CURLcode x = httpPost("https://jsonplaceholder.typicode.com/posts");
   cout << x;
-
 
 	return 0;
 }
