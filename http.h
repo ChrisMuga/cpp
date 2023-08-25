6a4e9bed743a0cf8b0a0b9e2d7d8006ff009e664
Chris Muga <chrismuga94@gmail.com>
2023-08-25 19:57:58 +0300

    remove unused fx

---
+++ http.h
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <curl/curl.h>
+ 
+static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp){
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+CURLcode httpGet(){
+  CURL *curl;
+  CURLcode res;
+  string readBuffer;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/users");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+  }
+  return res;
+}
+
+
+CURLcode httpPost(string url){
+  CURL *curl;
+  CURLcode res;
+
+  /* In windows, this will init the winsock stuff */
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* get a curl handle */
+  curl = curl_easy_init();
+  
+  if(curl) {
+    /* First set the URL that is about to receive our POST. This URL can
+       just as well be an https:// URL if that is what should receive the
+       data. */
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    
+    /* Set headers */
+    struct curl_slist* headers = NULL;
+    curl_slist_append(headers, "Content-Type: application/json");
+
+    /* Now specify the POST data */
+    string data = "{\"userId\":\"1\",\"title\":\"Software Engineer\",\"id\":\"300\",\"body\": \"Hello World\" }";
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data.length());
+ 
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+
+  return res;
+}
