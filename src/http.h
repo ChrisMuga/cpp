c536c5718b1ed8cd3f11d5427cb8d5b0368174dd
Chris Muga <chrismuga94@gmail.com>
2023-08-25 20:36:44 +0300

    remove defunct fx

--- src/http.h
+++ src/http.h
@@ -6,10 +6,6 @@ static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
-string x(){
- return "Hello"; 
-}
-
 CURLcode httpGet(){
   CURL *curl;
   CURLcode res;
